fb9cbc2e36d1c50025580f08752a3d54fb0c16cb
aasspaassp <andresfilos94@gmail.com>
2024-09-07 22:57:54 -0600

    dev prod variables

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 docker infrastructure 
+
+ docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build 
+
+ docker exec -it node-docker-mongo-1 mongosh -u "root" -p "example" 
+
--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express")
+const { MongoClient } = require("mongodb");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express()
 
+const uri = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}?authSource=admin&retryWrites=true&writeConcern=majority`
+const client = new MongoClient(uri);
+
+
+
+async function run() {
+    try {
+      const database = client.db('sample_mflix');
+      const movies = database.collection('movies');
+      // Query for a movie that has the title 'Back to the Future'
+      const query = { title: 'The Favourite' };
+      const movie = await movies.findOne(query);
+      console.log(movie);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 app.get("/", (req,res)=>{
-    res.send("<h2>Salu300 mundo</h2>")
+    res.send("<h2>Sal00 mundo</h2>")
 })
 
 const port = process.env.PORT || 3000
