748270a8e76b0de877ce9b679860000f02024752
aasspaassp <andresfilos94@gmail.com>
2025-11-08 21:01:25 -0600

    please fogiveme

--- pythoncourse/app.py
+++ pythoncourse/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template_string
-
+import os
 from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
@@ -141,4 +140,4 @@ def home():
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host='0.0.0.0', port=int(os.environ.get("FLASK_SERVER_PORT", 5050)), debug=True)
